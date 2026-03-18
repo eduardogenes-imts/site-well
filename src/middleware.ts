ac46f508089b9b52b53b7e2d7f2cf49e5f7c6034
eduardo genes <eduardogenes95@gmail.com>
2026-03-18 01:50:32 -0300

    feat: adicionar tela de bloqueio e middleware

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const isAuth = request.cookies.has('site-auth')
+  const isLoginPage = request.nextUrl.pathname === '/login'
+
+  if (!isAuth && !isLoginPage) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if (isAuth && isLoginPage) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - imagens da pasta public (images/)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|images/).*)',
+  ],
+}
